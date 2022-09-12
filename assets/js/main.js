d7ada730eb89a5495d222ea161051a71988ef02a
kartikey110813@gmail.com <kartikey110813@gmail.com>
2022-09-12 21:07:20 +0530

    products Page, contact, and minor UI changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
   /**
    * Preloader
    */
@@ -125,6 +134,15 @@ document.addEventListener('DOMContentLoaded', () => {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
+  });
+
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
   });
 
   /**
